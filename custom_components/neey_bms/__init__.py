2ec70cacb6c26452fe9b53a0080b015aab61cf8a
Ruud1977 <zakelijk@mail-thuis.nl>
2026-01-03 23:44:48 +0100

    Update __init__.py

--- custom_components/neey_bms/__init__.py
+++ custom_components/neey_bms/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import ble_client
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_BLE_CLIENT_ID
 
 AUTO_LOAD = ["ble_client"]
 
@@ -10,11 +10,11 @@ NEEYBMS = neey_bms_ns.class_("NEEYBMS", cg.Component, ble_client.BLEClientNode)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(NEEYBMS),
-    cv.Required("ble_client_id"): cv.use_id(ble_client.BLEClient)
+    cv.Required(CONF_BLE_CLIENT_ID): cv.use_id(ble_client.BLEClient),
 })
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var)
-    ble = yield cg.get_variable(config["ble_client_id"])
+    ble = yield cg.get_variable(config[CONF_BLE_CLIENT_ID])
     cg.add(var.set_ble_client(ble))
